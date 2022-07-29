a53e181a23e1cd6ddf6776a5a3ce9586b753a680
Danushan Ravendran <it20051648@my.sliit.lk>
2022-07-29 22:49:18 +0530

    first commit

---
+++ lib/features/number_trivia/domain/entities/number_triivia.dart
@@ -0,0 +1,10 @@
+import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
+
+abstract class NumberTrivia extends Equatable {
+  final String text;
+  final int number;
+
+  NumberTrivia({required this.text, required this.number})
+      : super ();
+}
